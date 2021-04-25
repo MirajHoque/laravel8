e75d172a9da1cc74cd8ed537fd02fe119b7ed6fd
MirajHoque <a.h.miraj2031@gmail.com>
2021-04-26 00:14:50 +0600

    blade template heade and footer

---
+++ resources/views/header.blade.php
@@ -0,0 +1,13 @@
+<header class="header">
+  <h1>header view</h1>
+</header>
+
+<style>
+  .header{
+    background-color: skyblue;
+    color: whitesmoke;
+    height: 100px;
+    width: 100%;
+    text-align: center;
+  }
+</style>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,30 +1,13 @@
+@include('header')
 <h1>users page</h1>
-{{--
-  {{ 10+10 }}
-  <h2>total number of registered users are {{ count($registerdUsers) }}</h2>
-  --}}
-
-{{--using condition statement--}}
-
-{{--
-
-@if($userName=='Mita')
-<h3>Hi {{ $userName }}</h3>
-@elseif($userName=='Mila')
-<h3>Hello {{ $userName }}</h3>
-@else
-<h3>unknown user</h3>
-@endif  
-
---}}
-
-{{--using loops--}}
-
-@for($i=0; $i<10; $i++)
-<span>{{ $i }}</span>
-@endfor
-
+@include('inner')
 @foreach ($registeredUsers as $element)
 <h3>{{ $element }}</h3>
     
 @endforeach
+
+@csrf
+<script>
+  var data=@json($registeredUsers);
+  console.log(data[0]);
+</script>
