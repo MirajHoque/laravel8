c67fd8883c822d4a6848975020b5e874cbadcdb4
MirajHoque <a.h.miraj2031@gmail.com>
2021-04-29 23:09:05 +0600

    form validation
    description of directives and methods

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//provide an object oriented way that is used for obatain a instance of current HTTP request via dependency injection
 
 class UsersController extends Controller
 {
-    function getData(Request $req){
+    function getData(Request $req){ //Request(class) $req(obj)
+        $req->validate([
+            'userName'=>'required | max:10',
+            'password'=>'required | min:8'
+        ]);
         return $req->input();
     }
 }
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,8 +1,34 @@
 <h1>User LogIn </h1>
+{{--
+@if($errors->any())
+any(): boolean method return true if there is any error or return false if error not found.
 
+@foreach ($errors->all() as $element)
+all(): return an array of message for all fields.
+<li>{{ $element }}</li>
+    
+@endforeach
+
+@endif
+--}}
+{{-- 
+  $errors: instance of Illuminate\Support\MessageBag
+--}}
 <form action="users" method="POST">
 @csrf
-<input type="text" name="userName" placeholder="enter user id" /> <br> <br>
-<input type="text" name="password" placeholder="enter user password" /> <br> <br>
+<input type="text" name="userName" placeholder="enter user id" /> <br> 
+<span style="color: red;">
+  @error('userName') {{ $message }} 
+  <!-- here userName is the name of the attribute-->
+  @enderror
+</span>
+<br>
+<input type="text" name="password" placeholder="enter user password" /> <br>
+<span style="color: red;">
+  @error('password') {{ $message }}
+  <!-- here password is the name of the attribute-->
+  @enderror
+</span>
+<br>
 <button type="submit">Sign In</button>
 </form>
