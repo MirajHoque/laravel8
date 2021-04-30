500107733249e43ba78bbf90dbc4a36eb05858e1
MirajHoque <a.h.miraj2031@gmail.com>
2021-05-01 01:37:30 +0600

    database configuration and retrive data using controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user', [UserController::class, 'index']);
