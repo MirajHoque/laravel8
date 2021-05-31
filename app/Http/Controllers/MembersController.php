55c0d3b524317a36b0e118fca74fe42c12edbe1e
MirajHoque <a.h.miraj2031@gmail.com>
2021-05-31 23:14:24 +0600

    aggregate methods

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -8,36 +8,19 @@ use Illuminate\Support\Facades\DB;
 class MembersController extends Controller
 {
     function dbOperations(){
-        //retrive date
-        //return DB::table('members')->where('address',"dhaka")->get();
-       
-        //find date
-        // return (array) DB::table('members')->find(4);
-       
-        //count data
-        //return DB::table('members')->count();
+        //sum
+        //return DB::table('members')->sum('id');
 
-       //insert
-      
-      /* return DB::table('members')->insert(
-           [
-               'name'=> "Zahra Tahfim",
-               'email'=> "tahfim@gmail.com",
-               'address'=> "Chittagong"
-           ]
-           );
-      */
-      
-      //update
+        //average
+        //return DB::table('members')->avg('id');
 
-      /*
-      return DB::table('members')->where('id',20)->update(
-          [
-            'name'=> "Jannatul Zahra"
-          ]);
-      */
-      
-      //delete
-      return DB::table('members')->where('id',20)->delete();
+        //max
+        //return DB::table('members')->max('id');
+
+        //min
+        //return DB::table('members')->min('id');
+
+        //count
+        return DB::table('members')->count('id');
     }
 }
