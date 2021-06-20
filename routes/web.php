dad908bee8f295c25a1618f42c33420d65b1af83
MirajHoque <a.h.miraj2031@gmail.com>
2021-06-20 22:06:41 +0600

    fluentString

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+$data="hi, let's learn laravel8";
+/*
+
+$data=Str::ucfirst($data); //captailize first letter
+$data= Str::replaceFirst('Hi', 'Hello', $data); //replace 
+$data= Str::camel($data); //camel case
+echo $data;
+
+*/
+
+$value= Str::of($data)
+        ->ucfirst($data)
+        ->replaceFirst('Hi', 'Hello', $data)
+        ->camel($data);
+
+echo $value;
 
 Route::get('/', function () {
     return view('welcome');
