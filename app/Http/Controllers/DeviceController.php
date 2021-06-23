6db785dfe4d84b070eb0b3787f92980fe1aec926
MirajHoque <a.h.miraj2031@gmail.com>
2021-06-24 02:08:49 +0600

    delete method api

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -22,4 +22,32 @@ class DeviceController extends Controller
             return ["Result"=>"Operation failed"];
         }
     }
+    function update(Request $req){
+        $device= Device::find($req->id);
+        $device->name= $req->name;
+        $device->member_id= $req->member_id;
+        $result= $device->save();
+        if($result)
+        {
+            return ["Result"=>"Data has been updated"];
+        }
+        
+        else
+        {
+            return ["Result"=>"Update operation failed"];
+        }
+    }
+    function delete($id){
+        $device= Device::find($id);
+        $result= $device->delete();
+        if($result)
+        {
+            return ["Result"=>"Data has been deleted"];
+        }
+        else
+        {
+            return ["Result"=>"Delete operationn failed"];
+        }
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('add', [DeviceController:: class, 'add']);
+Route::put('update', [DeviceController::class, 'update']);
+Route::delete('delete/{id}', [DeviceController::class, 'delete']);
