7e9918bdb607860e7ae1293265d5704d9e874bc3
MirajHoque <a.h.miraj2031@gmail.com>
2021-06-23 22:52:39 +0600

    post method api

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Device;
+
+class DeviceController extends Controller
+{
+    function add(Request $req){
+        $device= new Device;
+        $device->name= $req->name;
+        $device->member_id= $req->member_id;
+        $result= $device->save();
+        if($result)
+        {
+            return ["Result"=>"Data has been saved"];
+        }
+        
+        else
+        {
+            return ["Result"=>"Operation failed"];
+        }
+    }
+}
